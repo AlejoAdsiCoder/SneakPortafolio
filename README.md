89c21e88972d28eafa967b03ca9ad41c43bd0cf5
Alejandro <leho.patino@gmail.com>
2022-04-02 16:15:37 -0500

    General template

---
+++ README.md
@@ -0,0 +1,1 @@
+# SneakPortafolio
---
+++ js/main.js
@@ -0,0 +1,37 @@
+const gallery = document.querySelector(".gallery .gallery__grid");
+const access_key = 'URsUbqNo-_8C9C3DRGKvsrSnPhAFzisz2nxCAlnE17Q';
+const btnBurguer = document.querySelector(".navbar .navbar__hamburger")
+const navbar = document.querySelector(".navbar__nav");
+
+function photoGallery({ urls }) {
+    console.log(urls.regular)
+    gallery.innerHTML += `
+        <div class="gallery__photo">
+         <img src="${urls.regular}" class="gallery__image" />
+         <div class="gallery__overlay">
+                <span>creative logo</span>
+                <p>Branding</p>
+         </div>
+        </div>
+     `;
+    return gallery;
+}
+
+btnBurguer.addEventListener("click", function() {
+    btnBurguer.classList.toggle("hamburger--3dx");
+    btnBurguer.classList.toggle("is-active");
+    navbar.classList.toggle("navbar__nav--mobile");
+})
+
+function getImages() {
+    fetch(`https://api.unsplash.com/photos/random?client_id=${access_key}&count=10`)
+        .then(res => res.json())
+        .then(json => 
+            {
+                json.map(data => 
+                    photoGallery(data)).join("\n");
+            }
+        )
+}
+
+getImages();
