c0944d8821aa9eeb8f48883de9861cd64fce583c
Alejandro <leho.patino@gmail.com>
2022-04-02 18:36:33 -0500

    Gallery and fonts full

--- js/main.js
+++ js/main.js
@@ -3,25 +3,24 @@ const access_key = 'URsUbqNo-_8C9C3DRGKvsrSnPhAFzisz2nxCAlnE17Q';
 const btnBurguer = document.querySelector(".navbar .navbar__hamburger")
 const navbar = document.querySelector(".navbar__nav");
 
+btnBurguer.addEventListener("click", function() {
+    btnBurguer.classList.toggle("hamburger--3dx");
+    btnBurguer.classList.toggle("is-active");
+    navbar.classList.toggle("navbar__nav--mobile");
+})
+
 function photoGallery({ urls }) {
-    console.log(urls.regular)
     gallery.innerHTML += `
         <div class="gallery__photo">
-         <img src="${urls.regular}" class="gallery__image" />
+         <img src="${urls.regular}" class="gallery__image" alt="imagen de portafolio" />
          <div class="gallery__overlay">
-                <span>creative logo</span>
+                <span>creative logo</span>   
                 <p>Branding</p>
          </div>
         </div>
      `;
     return gallery;
 }
-
-btnBurguer.addEventListener("click", function() {
-    btnBurguer.classList.toggle("hamburger--3dx");
-    btnBurguer.classList.toggle("is-active");
-    navbar.classList.toggle("navbar__nav--mobile");
-})
 
 function getImages() {
     fetch(`https://api.unsplash.com/photos/random?client_id=${access_key}&count=10`)
